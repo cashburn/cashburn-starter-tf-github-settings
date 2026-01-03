d1161a6aa551b624389ca6b722886c63c758c409
Colin Ashburn <cashburn26@gmail.com>
2026-01-03 15:36:39 -0600

    Add releases ruleset

--- github-settings/main.tf
+++ github-settings/main.tf
@@ -48,3 +48,35 @@ resource "github_repository_ruleset" "default" {
     bypass_mode = "always"
   }
 }
+
+resource "github_repository_ruleset" "releases" {
+  name        = "releases-ruleset"
+  repository  = github_repository.repo.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = [
+        "refs/heads/releases/*"
+      ]
+      exclude = []
+    }
+  }
+
+  rules {
+    required_linear_history = true
+
+    pull_request {
+      required_approving_review_count = 1
+      dismiss_stale_reviews_on_push   = true
+      require_code_owner_review       = true
+    }
+    
+  }
+  bypass_actors {
+    actor_id    = 5 # Anyone with Repository Admin role
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+  }
+}
