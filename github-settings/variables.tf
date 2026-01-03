5f4bc7afe98da327b925d16dc13fffeb05d3840d
Colin Ashburn <cashburn26@gmail.com>
2026-01-03 11:14:36 -0600

    cd: Use a GitHub PAT to authenticate terraform (#1)

--- github-settings/variables.tf
+++ github-settings/variables.tf
@@ -1,3 +1,9 @@
+variable "github_token" {
+  type      = string
+  description = "GitHub PAT. Provide this as a TF_VAR using GitHub Actions Secrets."
+  sensitive = true
+}
+
 variable "github_owner" {
   type        = string
   description = "GitHub organization or user"
